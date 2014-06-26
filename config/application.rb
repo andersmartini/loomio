06779a4ef6961ba15451b4bf61ea53b6dbeec50b
Elias <elias.westerberg@gmail.com>
2014-06-26 12:16:11 +0200

    default locale sv and a couple of text fixes

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module Loomio
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :sv
 
     config.i18n.fallbacks = true
 
