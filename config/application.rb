368570fdd51f30cecabfdd89106ff0b2cc145d0b
Elias <elias.westerberg@gmail.com>
2014-06-26 16:59:51 +0200

    switching back from default locale sv due to bug

--- config/application.rb
+++ config/application.rb
@@ -40,8 +40,8 @@ module Loomio
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :sv
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.default_locale = :sv
 
     config.i18n.fallbacks = true
 
